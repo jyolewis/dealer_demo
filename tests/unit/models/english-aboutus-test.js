6cef771b77e6370f9b15154b3bf7590308236a92
Joseph <jyo.lewis@gmail.com>
2015-06-09 09:45:11 -0400

    Merged from Pm_wrk: Minor Redesign layout and CSS

---
+++ tests/unit/models/english-aboutus-test.js
@@ -0,0 +1,12 @@
+import { moduleForModel, test } from 'ember-qunit';
+
+moduleForModel('english-aboutus', 'Unit | Model | english aboutus', {
+  // Specify the other units that are required for this test.
+  needs: []
+});
+
+test('it exists', function(assert) {
+  var model = this.subject();
+  // var store = this.store();
+  assert.ok(!!model);
+});
