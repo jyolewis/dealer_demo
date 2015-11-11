a8a857d6c8205713c09c258d31487e64e0be75d1
reaper1993 <papeethan17@gmail.com>
2015-11-11 07:34:16 -0500

    commited after adding 4 modified files: bower.json, dashboard.js/hbs, login.hbs

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -2,15 +2,15 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+/*export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model: function() {
     return this.store.find('product');
   }
-});
+});*/
 
-// <!-- FOR TESTING PURPOSES -->
-// export default Ember.Route.extend({
-//   model: function() {
-//     return this.store.find('product');
-//   }
-// });
+ //<!-- FOR TESTING PURPOSES -->
+ export default Ember.Route.extend({
+   model: function() {
+     return this.store.find('product');
+   }
+ });
